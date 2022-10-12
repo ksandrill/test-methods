d4065f622c87c2bc139f1954dd4391c2bd91ed5b
Timur Zolotukhin <timur.zolotukhin@diabolocom.com>
2022-10-12 22:49:15 +0700

    refactoring for tests

--- tm-backend/src/test/java/org/nsu/fit/tm_backend/impl/CustomerServiceImplTest.java
+++ tm-backend/src/test/java/org/nsu/fit/tm_backend/impl/CustomerServiceImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.nsu.fit.tm_backend.repository.Repository;
@@ -17,7 +18,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-// Лабораторная 2: покрыть юнит тестами класс CustomerManager на 100%.
+// Лабораторная 2: покрыть unit тестами класс CustomerServiceImpl на 100%.
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceImplTest {
     @Mock
@@ -26,12 +27,10 @@ class CustomerServiceImplTest {
     @InjectMocks
     private CustomerServiceImpl customerService;
 
-    private CustomerPojo createCustomerInput;
-
     @Test
     void testCreateCustomer() {
-        // настраиваем mock.
-        createCustomerInput = new CustomerPojo();
+        // arrange: готовим входные аргументы и настраиваем mock'и.
+        CustomerPojo createCustomerInput = new CustomerPojo();
         createCustomerInput.firstName = "John";
         createCustomerInput.lastName = "Wick";
         createCustomerInput.login = "john_wick@example.com";
@@ -48,10 +47,10 @@ class CustomerServiceImplTest {
 
         when(repository.createCustomer(createCustomerInput)).thenReturn(createCustomerOutput);
 
-        // Вызываем метод, который хотим протестировать
+        // act: вызываем метод, который хотим протестировать.
         CustomerPojo customer = customerService.createCustomer(createCustomerInput);
 
-        // Проверяем результат выполенния метода
+        // assert: проверяем результат выполнения метода.
         assertEquals(customer.id, createCustomerOutput.id);
 
         // Проверяем, что метод по созданию Customer был вызван ровно 1 раз с определенными аргументами
@@ -73,6 +72,7 @@ class CustomerServiceImplTest {
 
     @Test
     void testCreateCustomerWithNullArgument_Right() {
+        // act-assert
         Exception exception = assertThrows(IllegalArgumentException.class, () ->
                 customerService.createCustomer(null));
         assertEquals("Argument 'customer' is null.", exception.getMessage());
@@ -80,13 +80,15 @@ class CustomerServiceImplTest {
 
     @Test
     void testCreateCustomerWithShortPassword() {
-        createCustomerInput = new CustomerPojo();
+        // arrange
+        CustomerPojo createCustomerInput = new CustomerPojo();
         createCustomerInput.firstName = "John";
         createCustomerInput.lastName = "Wick";
         createCustomerInput.login = "john_wick@example.com";
         createCustomerInput.pass = "123qwe";
         createCustomerInput.balance = 0;
 
+        // act-assert
         Exception exception = assertThrows(IllegalArgumentException.class, () -> customerService.createCustomer(createCustomerInput));
         assertEquals("Password is very easy.", exception.getMessage());
     }
--- tm-backend/src/test/java/org/nsu/fit/tm_backend/impl/StatisticServiceImplTest.java
+++ tm-backend/src/test/java/org/nsu/fit/tm_backend/impl/StatisticServiceImplTest.java
@@ -1,5 +1,7 @@
 package org.nsu.fit.tm_backend.impl;
 
+// Лабораторная 2: покрыть unit тестами класс StatisticServiceImpl на 100%.
+// Чтобы протестировать метод calculate() используйте Mockito.spy(statisticService) и переопределите метод
+// calculate(UUID customerId) чтобы использовать стратегию "разделяй и властвуй".
 public class StatisticServiceImplTest {
-    // Лабораторная 2: покрыть юнит тестами класс StatisticOperation на 100%.
 }
